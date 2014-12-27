1ad7dca57a9ddc691c18ad7eb783f373088d8710
ankurkumar84 <indianengg84@gmail.com>
2014-12-27 17:21:14 +0530

    Another test commit

--- src/HellowWorld.java
+++ src/HellowWorld.java
@@ -1,10 +1,14 @@
+import java.util.Scanner;
 
 /**
  * Created by Ankur on 12/27/2014.
  */
 public class HellowWorld {
 
-    public static void main(String[] args){
-        System.out.print("Hello World!");
+    public static void main(String[] args) {
+        System.out.print("Enter your name:");
+        Scanner input = new Scanner(System.in);
+
+        System.out.printf("Hello %s%n",input.nextLine() );
     }
 }
